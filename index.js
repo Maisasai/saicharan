5759cf81588cb9b93310dd36fee494804b4d8add
Maisasai <142029070+Maisasai@users.noreply.github.com>
2024-01-31 14:37:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let cart = document.getElementById("cart")
+let option = documnet.getElementById("option")
+
+option.onclick=function(){
+    let div = document.createElement("div")
+    option.appendchild(div)
+    let h2 = document.createElement("h2")
+    h2.textContent="shiva"
+    div.appendchild(h2)
+}
+
